9f4eaa5be8db2bffc090c59048141b719dfe4444
nfhfuhe <vova28072007@mail.ru>
2025-12-16 18:28:00 +0300

    1commit

---
+++ PizaStore.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PizaLibrary;
+
+abstract class PizaStore
+{
+    protected abstract function createPiza(string $type);
+    public function orderPiza(string $type)
+    {
+        $piza = $this->createPiza($type);
+    }
+}
